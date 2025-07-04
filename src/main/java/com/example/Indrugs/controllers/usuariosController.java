187460cdae17d438d6cc1e714645598f34dc2e64
Maris <marianabarretocas@gmail.com>
2025-07-03 19:28:12 -0500

    ejemplo de comits

--- src/main/java/com/example/Indrugs/controllers/usuariosController.java
+++ src/main/java/com/example/Indrugs/controllers/usuariosController.java
@@ -8,7 +8,7 @@
 ////import org.springframework.stereotype.Controller;
 ////import org.springframework.web.bind.annotation.GetMapping;
 ////
-//@Controller
+//@Control
 //public class usuariosController
 //
 //
